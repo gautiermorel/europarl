b73796406e8187e3f5a650666c7edf0973986b79
Gautier MOREL <gautier@bsit.com>
2020-04-16 19:52:08 +0200

    chore(): update europarl

--- src/components/AmendmentsList.vue
+++ src/components/AmendmentsList.vue
@@ -2,11 +2,17 @@
   <el-row type="flex" justify="center">
     <el-card class="box-card" style="width: 100%">
       <el-table ref="multipleTable" :data="amendments" empty-text="Chargement..." style="width: 100%" @selection-change="handleSelectionChange">
-        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column type="selection"></el-table-column>
         <el-table-column label="Amandement" width="220">
           <template slot-scope="scope">{{ scope.row.date }}</template>
         </el-table-column>
-        <el-table-column property="name" label="Name"></el-table-column>
+        <el-table-column property="name">
+          <template slot="header">
+            <el-row type="flex" justify="end">
+              <el-button type="success" icon="el-icon-download" :disabled="selection.length === 0" v-on:click="download()">Export XLSX</el-button>
+            </el-row>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
   </el-row>
@@ -35,28 +41,45 @@ export default {
       this.$emit("forwaredFromAmendments", checkedNames);
     },
     handleSelectionChange(amandements) {
-      console.log("amandement", amandements);
-      this.selection =
-        amandements && amandements.map(amandement => amandement.id);
-      console.log("this.selection", this.selection);
+      this.selection = amandements && amandements.map(amandement => amandement.id);
       this.$emit("forwaredFromAmendments", this.selection);
+    },
+    download: function() {
+      if (this.$route && this.$route.params && this.$route.params.id) this.voteId = this.$route.params.id;
+
+      this.$publicApi
+        .get(`/votes/${this.voteId}/export`, {
+          params: {
+            amendmentIds: this.checkedNames
+          },
+          responseType: "arraybuffer"
+        })
+        .then(res => {
+          var fileURL = window.URL.createObjectURL(new Blob([res.data]));
+          var fileLink = document.createElement("a");
+
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", `${this.voteId}.xlsx`);
+          document.body.appendChild(fileLink);
+
+          fileLink.click();
+        })
+        .catch(err => {
+          console.log('ERROR: AmendmentsList.vue#function - Error while downloading:', res);
+          this.$message.error('Oops, this is a error message.');
+        });
     }
   },
   mounted: function() {
-    if (this.$route && this.$route.params && this.$route.params.id)
-      this.voteId = this.$route.params.id;
+    if (this.$route && this.$route.params && this.$route.params.id) this.voteId = this.$route.params.id;
 
     this.$publicApi
       .get(`/votes/${this.voteId}/amendments`)
       .then(res => {
         this.amendments = (res && res.data && res.data.data) || [];
-        console.log("this.amendments=", this.amendments);
       })
       .catch(err => {
-        console.log(
-          "ERROR: AmendmentsList.vue#mounted - Error while getting amendments:",
-          err
-        );
+        console.log("ERROR: AmendmentsList.vue#mounted - Error while getting amendments:", err);
       });
   },
   watch: {}
