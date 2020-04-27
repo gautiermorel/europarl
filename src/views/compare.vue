b6e202c16f4dc6eeae39f72432d10c431097a496
Gautier MOREL <gautier@bsit.com>
2020-04-27 23:38:55 +0200

    feat(): add infos & search meps

--- src/views/compare.vue
+++ src/views/compare.vue
@@ -1,12 +1,22 @@
 <template>
-	<el-row type="flex" justify="center">
-		<el-table ref="multipleTable" :data="votes" stripe empty-text="Chargement..." style="width: 100%">
-			<el-table-column type="selection" width="55"></el-table-column>
-			<el-table-column v-for="property in meps" :key="property" :property="property" :label="property" min-width="100">
-				<template slot-scope="scope">{{scope.row[property]}}</template>
-			</el-table-column>
-		</el-table>
-	</el-row>
+	<el-col>
+		<el-row type="flex" justify="center" style="padding: 20px; width: 100%">
+			<el-select style="width: 100%" v-model="value" multiple filterable remote reserve-keyword placeholder="Please enter a keyword" :remote-method="remoteMethod" :loading="loading">
+				<el-option v-for="item in options" :key="item.mepid" :label="item.fullName" :value="item.mepid"></el-option>
+			</el-select>
+		</el-row>
+		<el-row type="flex" justify="center">
+			<el-table ref="multipleTable" :data="votes" stripe empty-text="Chargement..." style="width: 100%">
+				<el-table-column type="selection" width="55"></el-table-column>
+				<el-table-column v-for="property in meps" :key="property" :property="property" :label="property" min-width="100">
+					<template slot="header">
+						<el-row type="flex" justify="center">{{getHeader(property)}}</el-row>
+					</template>
+					<template slot-scope="scope">{{scope.row[property]}}</template>
+				</el-table-column>
+			</el-table>
+		</el-row>
+	</el-col>
 </template>
 
 <i18n>{}</i18n>
@@ -18,19 +28,66 @@ export default {
 	props: {},
 	data: function() {
 		return {
-			votes: []
+			votes: [],
+			options: [],
+			value: [],
+			list: [],
+			loading: false,
+			states: []
 		};
 	},
 	computed: {
 		meps: function() {
 			let [vote = {}] = this.votes || [];
-			return Object.keys(vote).reverse();
+			return Object.keys(vote)
+				.filter(k => k !== "_id" && k !== "meps")
+				.reverse();
 		}
 	},
-	methods: {},
+	methods: {
+		getHeader: function(property) {
+			if (property === "_id" || property === "title") return property;
+			let [vote = {}] = this.votes || [];
+			let mep = vote.meps.find(mep => `${mep.UserID}` === `${property}`) || {};
+			return mep.Name.full;
+		},
+		remoteMethod: function(query) {
+			if (query !== "") {
+				this.loading = true;
+
+				this.$publicApi
+					.get("/meps", {
+						params: {
+							search: query
+						}
+					})
+					.then(res => {
+						this.loading = false;
+						let meps = (res && res.data) || {};
+						this.options = meps || [];
+					})
+					.catch(err => {
+						console.log("ERROR: sittingsList.vue#mounted - Error while getting sittings:", err);
+					});
+
+				// setTimeout(() => {
+				// 	this.loading = false;
+				// 	this.options = this.list.filter(item => {
+				// 		return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+				// 	});
+				// }, 200);
+			} else {
+				this.options = [];
+			}
+		}
+	},
 	mounted: function() {
+		this.list = this.states.map(item => {
+			return { value: `value:${item}`, label: `label:${item}` };
+		});
+
 		let mepids = this.$route.query.mepids;
-		mepids = (Array.isArray(mepids) && mepids) || [mepids];
+		mepids = (mepids && ((Array.isArray(mepids) && mepids) || [mepids])) || null;
 
 		let search = this.$route.query.search;
 
